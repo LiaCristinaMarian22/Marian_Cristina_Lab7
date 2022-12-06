fa9a45243f2de830372fb746435057b2d502033d
LiaCristinaMarian22 <cristinamarian22@gmail.com>
2022-12-06 15:45:43 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,28 @@
-﻿namespace Marian_Cristina_Lab7;
+﻿using System;
+using Marian_Cristina_Lab7.Data;
+using System.IO;
+using Marian_Cristina_Lab7.Data;
+
+namespace Marian_Cristina_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
