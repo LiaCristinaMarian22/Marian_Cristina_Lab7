263a04d8cfa392365c4e1395892421b423464dc4
LiaCristinaMarian22 <cristinamarian22@gmail.com>
2022-12-13 16:51:04 +0200

    Laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,11 +1,32 @@
-namespace Marian_Cristina_Lab7;
 using Marian_Cristina_Lab7.Models;
+using Microsoft.Maui.Controls;
+
+namespace Marian_Cristina_Lab7;
 public partial class ListPage : ContentPage
 {
 	public ListPage()
 	{
-		InitializeComponent();
+        InitializeComponent();
 	}
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+       
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+
+namespace Marian_Cristina_Lab7.Models
+{
+    public class Product
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        
+        [OneToMany]
+        public List<ListProduct> ListProducts { get; set; }
+
+    }
+}
