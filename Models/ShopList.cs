227daa4bbb34f9a8f2ad113a02db02ea31f21d9e
LiaCristinaMarian22 <cristinamarian22@gmail.com>
2022-12-21 19:19:39 +0200

    Laborator10

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -10,8 +10,6 @@ namespace Marian_Cristina_Lab7.Models
     public class ShopList
     {
         [PrimaryKey, AutoIncrement]
-        public int ID { get; set; }
-
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
